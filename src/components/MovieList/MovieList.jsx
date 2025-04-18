9eb7071d1397fdf3671227d583a4d5c403353975
Anna Onyshchenko <auonishchenko.com>
2025-04-18 20:01:08 +0300

    add task solution

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,7 @@ import './MovieList.scss';
 export const MovieList = ({ movies }) => (
   <div className="movies">
     {movies.map(movie => (
-      <MovieCard key={movie.imdbId} />
+      <MovieCard movie={movie} />
     ))}
   </div>
 );
